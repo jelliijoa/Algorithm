773ca521829a8ac7b83e83c953a6fb38bc8aa965
YOONJI <jelliijoa@gmail.com>
2024-08-04 11:54:05 +0900

    [Easy] Title: 101. Symmetric Tree, Time: 24 ms (9.43%), Memory: 11.7 MB (71.62%) - LeetHub

--- LeetCode/Easy/0101-symmetric-tree/0101-symmetric-tree.py
+++ LeetCode/Easy/0101-symmetric-tree/0101-symmetric-tree.py
@@ -1,29 +1,17 @@
 # Definition for a binary tree node.
 class TreeNode(object):
-     def __init__(self, val=0, left=None, right=None):
-         self.val = val
-         self.left = left
-         self.right = right
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution(object):
     def isSymmetric(self, root):
-        if not root:
-            return True
-        
-        stack = [(root.left, root.right)]
-
-        while stack:
-            left, right = stack.pop()
-
-            # 양쪽 모두 없으면 대칭
-            if not left and not right:
-                continue
-            
-            # left, right 중 하나만 존재하면 or 양쪽 값이 다르면 비대칭
-            if (not left or not right) or (left.val != right.val):
+        def check(left, right):
+            if left is None and right is None:
+                return True
+            if left is None or right is None:
                 return False
-            
-            stack.append((left.left, right.right))
-            stack.append((left.right, right.left))
+            return (left.val == right.val) and check(left.right, right.left) and check(left.left, right.right)
 
-        return True
+        return check(root.left, root.right) if root else True
