7f689043a65d6624b2a1d0a0a6cff5327be8578a
YOONJI <jelliijoa@gmail.com>
2024-08-06 18:13:44 +0900

    [level 1] Title: 최소직사각형, Time: 5.25 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,3 +1,3 @@
 def solution(sizes):
     # 각 리스트 max의 최댓값과 각 리스트 min의 최댓값을 곱해서 리턴
-    return max(max(s) for s in sizes) * max(min(s) for s in sizes)
+    return max(max(w, h) for w, h in sizes) * max(min(w, h) for w, h in sizes)
