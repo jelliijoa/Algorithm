aa83b207a15f0886d501136b27e8ab8cbb4fdc8a
YOONJI <jelliijoa@gmail.com>
2024-08-12 16:44:57 +0900

    [Easy] Title: 118. Pascal's Triangle, Time: 19 ms (30.34%), Memory: 11.7 MB (32.69%) - LeetHub

--- LeetCode/Easy/0118-pascals-triangle/0118-pascals-triangle.py
+++ LeetCode/Easy/0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,5 +1,18 @@
-from math import comb
-
 class Solution(object):
-    def generate(self, numRows: int) -> List[List[int]]:
-        return [[comb(n, k) for k in range(n + 1)] for n in range(numRows)]
+    def generate(self, numRows):
+        pascal = []
+
+        for i in range(numRows):
+            # pascal [[1], [1, 1], [1, 2, 1]]
+            # i = 3 / row [1, 1, 1, 1]
+            row = [1] * (i + 1)
+
+            for j in range(1, i):
+                # j = 1, 2
+                # j = 1 -> pascal[2][0] + pascal[2][1] -> row [1, 3, 1, 1]
+                # j = 2 -> pascal[2][1] + pascal[2][2] -> row[1, 3, 3, 1]
+                row[j] = pascal[i-1][j-1] + pascal[i-1][j]
+
+            pascal.append(row)
+        
+        return pascal
